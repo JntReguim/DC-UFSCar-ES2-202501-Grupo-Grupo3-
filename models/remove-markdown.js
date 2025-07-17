2d5f8bac55f0b4c5f4f2c2e75607c40706d94a8f
Murilo Ramos <muriloeduardo6@gmail.com>
2025-07-17 15:24:06 -0300

    fix: testes

--- models/remove-markdown.js
+++ models/remove-markdown.js
@@ -5,7 +5,7 @@ import logger from 'infra/logger';
 
 export default function customRemoveMarkdown(md, options = {}) {
   options.oneLine = Object.hasOwn(options, 'oneLine') ? options.oneLine : true;
-  options.throwError = Object.hasOwn(options, 'logError') ? options.throwError : true;
+  options.throwError = Object.hasOwn(options, 'throwError') ? options.throwError : true;
   options.trim = Object.hasOwn(options, 'trim') ? options.trim : true;
 
   let output = md || '';
